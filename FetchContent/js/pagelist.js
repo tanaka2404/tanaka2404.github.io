d58b1bc024e910dc1eb564cbae6f8734bd1d0eb1
tanaka2404 <souta_ta0204@icloud.com>
2025-07-18 23:29:10 +0900

    edit

--- FetchContent/js/pagelist.js
+++
@@ -1,9 +0,0 @@
-var myXml = new XMLHttpRequest();
-myXml.onreadystatechange = function() {
-    if ((myXml.readyState === 4) && (myXml.status === 200)) {
-        document.getElementById("pageList").innerHTML = myXml.responseText;
-        // ロード後の処理を記述
-    }
-}
-myXml.open("GET", "https://raw.githubusercontent.com/tanaka2404/tanaka2404.github.io/refs/heads/main/FetchContent/header.html", true);  // 元URL 
-myXml.send(null);
