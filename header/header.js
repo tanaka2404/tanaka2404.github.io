d4e7af10cc88a91b96dc64b136e45cf0ffaf99b0
tanaka2404 <souta_ta0204@icloud.com>
2025-07-18 17:55:04 +0900

    a

--- header/header.js
+++ header/header.js
@@ -6,5 +6,5 @@ myXml.onreadystatechange = function() {
         // ロード後の処理を記述
     }
 }
-myXml.open("GET", "https://raw.githubusercontent.com/tanaka2404/tanaka2404.github.io/refs/heads/main/sidebar/sidebar.html", true);  // 元URL 
+myXml.open("GET", "https://raw.githubusercontent.com/tanaka2404/tanaka2404.github.io/refs/heads/main/header/header.html", true);  // 元URL 
 myXml.send(null);
