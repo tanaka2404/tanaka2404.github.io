29a8e919ac7facd90ed5ab1e957f6da2602de9a8
tanaka2404 <souta_ta0204@icloud.com>
2025-07-18 23:19:31 +0900

    edit file name

---
+++ FetchContent/js/header.js
@@ -0,0 +1,9 @@
+var myXml = new XMLHttpRequest();
+myXml.onreadystatechange = function() {
+    if ((myXml.readyState === 4) && (myXml.status === 200)) {
+        document.getElementById("headerSidebar").innerHTML = myXml.responseText;
+        // ロード後の処理を記述
+    }
+}
+myXml.open("GET", "https://raw.githubusercontent.com/tanaka2404/tanaka2404.github.io/refs/heads/main/header/header.html", true);  // 元URL 
+myXml.send(null);
