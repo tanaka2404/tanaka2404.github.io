6380c2fa826ef62918ffade114d9ceec4c16f1b6
tanaka2404 <souta_ta0204@icloud.com>
2025-07-19 11:44:12 +0900

    edit

--- FetchContent/js/header.js
+++ FetchContent/js/header.js
@@ -3,11 +3,14 @@ myXml.onreadystatechange = function() {
     if ((myXml.readyState === 4) && (myXml.status === 200)) {
         document.getElementById("header").innerHTML = myXml.responseText;
         // ロード後の処理を記述
-        let headerPageListToggle = document.querySelector('.pageListToggle');
-        headerPageListToggle.addEventListener('click', function(){
+        document.querySelector('.pageListToggle').addEventListener('click', function() {
             let headPageList = document.querySelector('.headPageList');
             headPageList.classList.toggle('pageListVisible');
         });
+        // headerPageListToggle.addEventListener('click', function(){
+        //     let headPageList = document.querySelector('.headPageList');
+        //     headPageList.classList.toggle('pageListVisible');
+        // });
     }
 }
 myXml.open("GET", "https://raw.githubusercontent.com/tanaka2404/tanaka2404.github.io/refs/heads/main/FetchContent/header.html", true);  // 元URL 
